526e73708af03ba7a1d3250f1b1d2e32297adb38
Marcell Vazquez-Chanlatte <mvc@linux.com>
2024-02-20 20:04:36 -0800

    feat: Introduce learning from demonstrations modality.

--- src/lstar_lm/lstar_lm.py
+++ src/lstar_lm/lstar_lm.py
@@ -51,3 +51,76 @@ def guess_dfa(positive,
     # 3. Run learner.
     learner = L.guess_dfa_sat if use_dfa_identify else L.guess_dfa_lstar
     return learner(positive, negative, set(alphabet), label, active_queries)
+
+
+def dfa_search_with_diss(*,
+                         alphabet,
+
+                         # Passive identification params.
+                         positive=(),
+                         negative=(),
+
+                         # LLM oracle params.
+                         desc="",
+                         llm_params=L.DEFAULT_PARAMS,
+                         llm_query_callback=lambda *_: None,
+                         llm_endpoint=L.DEFAULT_ENDPOINT,
+                         allow_unsure=True,
+
+                         # Active learning params.
+                         ce_search_depth=-1,
+                         random_iters=0,    # Only used for random search.
+                         use_random_search=True,
+                         active_queries=10,
+                         use_dfa_identify=True,
+
+                         # Demonstration learning parameters.
+                         to_chain,
+                         demonstrations,
+                         max_diss_iters: int,
+                         diss_params,  # Other DISS parameters.
+
+                         # OTHER
+                         verbose=False,
+              ):
+    try:
+        import diss
+        from diss.concept_classes.dfa_concept import DFAConcept
+    except ModuleNotFoundError as e:
+        print("Please install the DISS package.\npip install diss")
+        raise e
+
+    def llm_identifer(data, concept=None):
+        lang = guess_dfa(positive=data.positive,
+                         negative=data.negative, 
+                         alphabet=alphabet,
+                         desc=desc,
+                         ce_search_depth=ce_search_depth,
+                         verbose=verbose,
+                         allow_unsure=allow_unsure,
+                         llm_query_call_back=llm_query_callback,
+                         llm_endpoint=llm_endpoint,
+                         use_random_search=use_random_search,
+                         random_iters=random_iters,
+                         active_queries=active_queries,
+                         use_dfa_identify=use_dfa_identify)
+
+        return DFAConcept.from_dfa(lang)
+
+    diss_params = {
+        "demos": demonstrations,
+        "sgs_temp": 0.01,
+        "n_iters": max_diss_iters,
+        "surprise_weight": 1,
+        "reset_period": 30,
+        "size_weight": 1/80,
+        "example_drop_prob": 1/20,
+        "synth_timeout": 0,
+        "competency": lambda *_: 10,
+        "to_chain": to_chain,
+        "to_concept": llm_identifer,
+    } | diss_params
+
+    return diss.diss(**diss_params)
+
+__all__ = ['dfa_search_with_diss', 'guess_dfa']
